a589de4b2db0594d89f7906a8a351e9affe47fac
Juan Burich <burichojrs@gmail.com>
2021-03-02 22:28:05 -0300

    Modificado App.js con mercadoFree

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './ml.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Bienvenidos a MercadoFree, somos una idea original
         </p>
         <a
           className="App-link"
