9881a9d10a0e022891591a50eb1ad7290f72bbed
Juan Burich <burichojrs@gmail.com>
2021-04-11 19:03:41 -0300

    Me olvide de guardar :(

--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {Container,Row} from "react-bootstrap";
 import { useParams } from "react-router-dom";
-import products from "../products"
 import { ItemList } from "./ItemList";
 
 
