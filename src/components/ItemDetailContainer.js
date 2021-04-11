aca5ea76c3b3c34122154547ddc07c856ddfee2e
Juan Burich <burichojrs@gmail.com>
2021-04-11 19:01:36 -0300

    Ahora me decis que toma los warnings como errores.

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import {ItemDetail} from "../components/ItemDetail"
-import products from "../products"
+
 
 
 
